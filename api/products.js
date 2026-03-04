674dd17d03bcb4594825134750b6c0ec2053c265
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-04 13:21:04 +0700

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const response = await fetch("https://api.loyverse.com/v1.0/items", {
     headers: {
-      Authorization: "Bearer sk_live_xxxxxxxxxxx"
+      Authorization: "34b14f9c227247aaa1cb8af92921bf0b"
     }
   });
 
