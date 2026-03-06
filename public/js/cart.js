505d2abacc2602984c97fa0e54614429a507fa53
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 21:37:07 +0700

    Update cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,47 +1,21 @@
-import {state} from "./state.js"
-import {sendOrder} from "./api.js"
+export let cart=[]
 
-export function addCart(){
+export function addToCart(item){
 
-state.cart.push(state.currentProduct)
+cart.push(item)
+
+updateCart()
+
+}
+
+export function updateCart(){
 
 let total=0
 
-state.cart.forEach(i=>{
+cart.forEach(i=>{
 total+=i.price
 })
 
 document.getElementById("floatingTotal").innerText=total
-document.getElementById("floatingCart").style.display="block"
-
-document.getElementById("modal").classList.remove("show")
-
-}
-
-export function openCart(){
-
-let html=`<div class="modal-content">`
-
-html+="<h3>Cart</h3>"
-
-state.cart.forEach(c=>{
-html+=`${c.name}<br>`
-})
-
-html+=`<button id="orderBtn">Order</button>`
-html+=`</div>`
-
-let modal=document.getElementById("modal")
-
-modal.innerHTML=html
-modal.classList.add("show")
-
-document.getElementById("orderBtn").onclick=async()=>{
-
-await sendOrder(state.cart)
-
-alert("Order sent")
-
-}
 
 }
