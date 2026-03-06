f8957c17f3c82ae4c0479c3962dd8f3cd0c03a95
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 16:09:48 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,18 @@
-import {state} from "./state.js"
-import {getProducts} from "./api.js"
-import {sortProducts} from "./utils.js"
-import {renderCategory,renderMenu} from "./menu.js"
+import { loadMenu } from "./menu.js"
 
-async function init(){
-
-try{
+async function start(){
 
 await liff.init({
 liffId:"2009308319-2r1OXrGI"
 })
 
-let data = await getProducts()
+if(!liff.isLoggedIn()){
+liff.login()
+return
+}
 
-data = data.map(p=>{
-if(!p.category) p.category="999_อื่นๆ"
-return p
-})
-
-state.products = sortProducts(data)
-
-renderCategory()
-renderMenu(state.products)
-
-}catch(e){
-
-console.error(e)
-alert("โหลดเมนูไม่สำเร็จ")
+loadMenu()
 
 }
 
-}
-
-init()
+start()
