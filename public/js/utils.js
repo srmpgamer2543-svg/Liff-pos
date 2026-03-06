5c841ca5120f1afef6a753b7f684c6b32a40f824
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 15:30:45 +0700

    Update utils.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,1 +1,15 @@
+export function sortProducts(products){
 
+return products.sort((a,b)=>{
+
+let ca = a.category || ""
+let cb = b.category || ""
+
+let na = parseInt(ca.split("_")[0])
+let nb = parseInt(cb.split("_")[0])
+
+return na - nb
+
+})
+
+}
