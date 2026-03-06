1188ea9d4fada17764df3ea8bed382ed6d1b4654
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 16:01:39 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,24 +1,3 @@
-import {state} from "./state.js"
-import {openModal} from "./modal.js"
-
-export function renderCategory(){
-
-let el = document.getElementById("category")
-
-let cats = [...new Set(state.products.map(p=>p.category))]
-
-let html = ""
-
-cats.forEach(c=>{
-
-html+=`<button onclick="filterCategory('${c}')">${c.split("_")[1]}</button>`
-
-})
-
-el.innerHTML = html
-
-}
-
 export function renderMenu(products){
 
 const menu = document.getElementById("menu")
@@ -27,16 +6,27 @@ menu.innerHTML=""
 
 products.forEach(p=>{
 
-const card = document.createElement("div")
+const card=document.createElement("div")
 card.className="menu-card"
 
-card.innerHTML = `
+card.innerHTML=`
+
 <img src="${p.image_url || ''}">
+
 <div class="menu-info">
-<div class="menu-name">${p.name}</div>
-<div class="menu-price">${p.price} ฿</div>
+
+<div class="menu-name">
+${p.name}
 </div>
-<button class="add-btn">+</button>
+
+<div class="menu-price">
+${p.price} ฿
+</div>
+
+</div>
+
+<button class="add-btn" onclick="openProduct('${p.id}')">+</button>
+
 `
 
 menu.appendChild(card)
@@ -44,13 +34,3 @@ menu.appendChild(card)
 })
 
 }
-
-window.filterCategory=(cat)=>{
-
-let list = state.products.filter(p=>p.category==cat)
-
-renderMenu(list)
-
-}
-
-window.openModal=openModal
