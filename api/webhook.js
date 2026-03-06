30a0448116ffdf74fc76e479b28273505f267b16
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-07 00:57:37 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,8 +2,34 @@ export default async function handler(req, res) {
 
   if (req.method === "POST") {
 
-    console.log("LINE BODY:");
-    console.log(JSON.stringify(req.body, null, 2));
+    const events = req.body.events;
+
+    for (const event of events) {
+
+      if (event.type === "message" && event.message.type === "text") {
+
+        const replyToken = event.replyToken;
+
+        await fetch("https://api.line.me/v2/bot/message/reply", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.LINE_ACCESS_TOKEN}`
+          },
+          body: JSON.stringify({
+            replyToken: replyToken,
+            messages: [
+              {
+                type: "text",
+                text: "ระบบเชื่อมต่อสำเร็จ ✅"
+              }
+            ]
+          })
+        });
+
+      }
+
+    }
 
   }
 
