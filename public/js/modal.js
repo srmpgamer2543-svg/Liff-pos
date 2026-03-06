c5029310dd46cbff9cb5d5545c2d696116d7324d
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 21:38:57 +0700

    Update modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,67 +1,19 @@
-import {state} from "./state.js"
-import {sortModifiers} from "./utils.js"
-import {addCart} from "./cart.js"
+export function openModal(html){
 
-export function openProduct(id){
+const modal=document.getElementById("modal")
 
-let p = state.products.find(x=>x.id==id)
+modal.innerHTML=`
+<div class="modal-box">
+${html}
+</div>
+`
 
-state.currentProduct = p
-
-let mods = sortModifiers([...p.modifiers])
-
-let html=`<div class="modal-content">`
-
-html+=`<h3>${p.name}</h3>`
-
-mods.forEach(m=>{
-
-html+=`<div class="modifier-group">`
-html+=`<h4>${m.name}</h4>`
-html+=`<div class="modifier-grid">`
-
-m.options.forEach(o=>{
-
-if(m.name.includes('ท้อปปิ้ง')){
-
-html+=`
-<label class="option">
-<input type="checkbox" value="${o.id}">
-${o.name}
-</label>`
-
-}else{
-
-html+=`
-<label class="option">
-<input type="radio" name="${m.id}" value="${o.id}">
-${o.name}
-</label>`
+modal.style.display="flex"
 
 }
 
-})
+export function closeModal(){
 
-html+=`</div></div>`
-
-})
-
-html+=`<button id="addCartBtn">เพิ่มลงตะกร้า</button>`
-html+=`</div>`
-
-let modal=document.getElementById("modal")
-
-modal.innerHTML=html
-modal.classList.add("show")
-
-modal.onclick=()=>{
-modal.classList.remove("show")
-}
-
-document.querySelector(".modal-content").onclick=(e)=>{
-e.stopPropagation()
-}
-
-document.getElementById("addCartBtn").onclick=addCart
+document.getElementById("modal").style.display="none"
 
 }
