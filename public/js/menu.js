9d666b2086c4b352f81a3b2d6ae9c4f1f1fa78bb
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 15:01:30 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,28 +1,35 @@
 import {state} from "./state.js"
 import {cleanCategory,sortCategory} from "./utils.js"
-import {openProduct} from "./modal.js"
 
 export function renderCategory(){
 
 let cats=[...new Set(state.products.map(p=>p.category))]
+
 cats.sort(sortCategory)
 
 let html='<button onclick="renderMenu(state.products)">All</button>'
 
 cats.forEach(c=>{
+
 html+=`<button onclick="filterCat('${c}')">${cleanCategory(c)}</button>`
+
 })
 
 document.getElementById('category').innerHTML=html
 
 }
 
+
+
 export function filterCat(c){
 
 let list=state.products.filter(p=>p.category==c)
+
 renderMenu(list)
 
 }
+
+
 
 export function renderMenu(items){
 
@@ -31,19 +38,27 @@ let html=''
 items.forEach(p=>{
 
 html+=`
+
 <div class="card" onclick="openProduct('${p.id}')">
 
 <div class="card-img">
+
 <img src="${p.image_url||''}">
+
 <div class="add-btn">+</div>
+
 </div>
 
 <div class="card-body">
+
 <div class="name">${p.name}</div>
+
 <div class="price">${p.price} ฿</div>
+
 </div>
 
 </div>
+
 `
 
 })
@@ -51,6 +66,8 @@ html+=`
 document.getElementById('menu').innerHTML=html
 
 }
+
+
 
 window.renderMenu = renderMenu
 window.filterCat = filterCat
