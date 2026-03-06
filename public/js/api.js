5ea2991d52ed2410da3a08067930b3cce95f0752
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 17:51:13 +0700

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,9 +1,16 @@
 export async function getProducts(){
 
-let res = await fetch("/api/products")
-
-if(!res.ok) throw new Error("API ERROR")
-
+let res = await fetch('/api/products')
 return await res.json()
+
+}
+
+export async function sendOrder(cart){
+
+await fetch('/api/order',{
+method:'POST',
+headers:{'Content-Type':'application/json'},
+body:JSON.stringify(cart)
+})
 
 }
