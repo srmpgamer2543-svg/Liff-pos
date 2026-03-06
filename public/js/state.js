9fdc94ac48cbdfe28b52109e68548ce8fa7eefdf
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 14:19:48 +0700

    Update state.js

--- public/js/state.js
+++ public/js/state.js
@@ -1,3 +1,21 @@
-export let products=[]
-export let cart=[]
-export let currentProduct=null
+export const state = {
+  products: [],
+  cart: [],
+  currentProduct: null
+}
+
+export function setProducts(data){
+  state.products = data
+}
+
+export function setCurrentProduct(product){
+  state.currentProduct = product
+}
+
+export function addToCart(item){
+  state.cart.push(item)
+}
+
+export function clearCart(){
+  state.cart = []
+}
