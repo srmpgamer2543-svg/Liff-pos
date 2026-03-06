6225b64f1e05861e2b6fba6f08160e34cf5d509f
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 15:44:10 +0700

    Update cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,1 +1,33 @@
+import {state} from "./state.js"
 
+export function updateFloating(){
+
+let total = 0
+
+state.cart.forEach(c=>{
+total += c.price * c.qty
+})
+
+let el = document.getElementById("floatingCart")
+
+if(state.cart.length==0){
+
+el.style.display="none"
+
+}else{
+
+el.style.display="block"
+
+}
+
+document.getElementById("floatingTotal").innerText = total
+
+}
+
+export function openCart(){
+
+alert("cart")
+
+}
+
+window.openCart=openCart
