32377f31d6838bdeba7e02740d2f09f42ab70257
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-07 00:51:26 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,20 +1,7 @@
 export default async function handler(req, res) {
 
-  if (req.method === "POST") {
-    const body = req.body;
+  console.log("LINE EVENT:", JSON.stringify(req.body, null, 2));
 
-    console.log("LINE EVENT:", JSON.stringify(body, null, 2));
+  res.status(200).json({ message: "Webhook received" });
 
-    if (body.events && body.events.length > 0) {
-      const event = body.events[0];
-
-      if (event.source && event.source.groupId) {
-        console.log("GROUP ID:", event.source.groupId);
-      }
-    }
-
-    return res.status(200).json({ status: "ok" });
-  }
-
-  res.status(200).json({ message: "Webhook Ready" });
 }
