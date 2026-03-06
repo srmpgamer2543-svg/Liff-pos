868cf829869f1942cf96d757dc5f558e4849c383
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 17:54:27 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,9 @@ import {state} from "./state.js"
 import {getProducts} from "./api.js"
 import {sortProducts} from "./utils.js"
 import {renderCategory,renderMenu} from "./menu.js"
+import {openCart} from "./cart.js"
 
 async function init(){
-
-console.log("APP START")
 
 try{
 
@@ -13,11 +12,7 @@ await liff.init({
 liffId:"2009308319-2r1OXrGI"
 })
 
-console.log("LIFF READY")
-
 let data = await getProducts()
-
-console.log("API READY",data.length)
 
 data = data.map(p=>{
 if(!p.category) p.category="999_อื่นๆ"
@@ -27,19 +22,32 @@ return p
 state.products = sortProducts(data)
 
 renderCategory()
-
-renderMenu()
-
-console.log("RENDER DONE")
+renderMenu(state.products)
 
 }catch(e){
 
 console.error(e)
-
 alert("โหลดเมนูไม่สำเร็จ")
 
 }
 
 }
+
+document.getElementById("cartBtn").onclick=openCart
+document.getElementById("floatingCart").onclick=openCart
+
+window.addEventListener("scroll",()=>{
+
+let logo=document.getElementById("logoArea")
+
+if(window.scrollY>80){
+logo.style.transform="translateY(-100%)"
+logo.style.opacity="0"
+}else{
+logo.style.transform="translateY(0)"
+logo.style.opacity="1"
+}
+
+})
 
 init()
