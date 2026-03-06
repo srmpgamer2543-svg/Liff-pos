8c53bfea7e7fa351e22c4a83f8f066144b573912
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 21:36:27 +0700

    Update and rename state.js to config.js

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+export const config = {
+
+storeId:"YOUR_STORE_ID",
+
+token:"YOUR_LOYVERSE_TOKEN",
+
+api:"https://api.loyverse.com/v1.0"
+
+}
--- public/js/state.js
+++
@@ -1,7 +0,0 @@
-export const state = {
-
-products:[],
-cart:[],
-currentProduct:null
-
-}
