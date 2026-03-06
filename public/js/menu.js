e8b43d3d7b8b0e1ad79b8882d8df06c77f0bf781
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 15:43:15 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,1 +1,58 @@
+import {state} from "./state.js"
+import {openModal} from "./modal.js"
 
+export function renderCategory(){
+
+let el = document.getElementById("category")
+
+let cats = [...new Set(state.products.map(p=>p.category))]
+
+let html = ""
+
+cats.forEach(c=>{
+
+html+=`<button onclick="filterCategory('${c}')">${c.split("_")[1]}</button>`
+
+})
+
+el.innerHTML = html
+
+}
+
+export function renderMenu(list){
+
+let el = document.getElementById("menu")
+
+let html = ""
+
+list.forEach(p=>{
+
+html+=`
+
+<div class="card" onclick="openModal('${p.id}')">
+
+<img src="${p.image_url||''}">
+
+<div class="name">${p.name}</div>
+
+<div class="price">${p.price} ฿</div>
+
+</div>
+
+`
+
+})
+
+el.innerHTML = html
+
+}
+
+window.filterCategory=(cat)=>{
+
+let list = state.products.filter(p=>p.category==cat)
+
+renderMenu(list)
+
+}
+
+window.openModal=openModal
