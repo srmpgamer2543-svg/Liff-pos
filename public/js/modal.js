5c1b150a5d30a18cedc1645d401d3f902a3553a2
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-06 15:43:43 +0700

    Update modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,1 +1,44 @@
+import {state} from "./state.js"
+import {updateFloating} from "./cart.js"
 
+export function openModal(id){
+
+let p = state.products.find(x=>x.id==id)
+
+state.currentProduct = p
+
+let html = `
+<div class="modal-content">
+
+<h3>${p.name}</h3>
+
+<button onclick="addToCart()">เพิ่มลงตะกร้า</button>
+
+</div>
+`
+
+let modal = document.getElementById("modal")
+
+modal.innerHTML = html
+modal.classList.add("show")
+
+}
+
+export function addToCart(){
+
+let p = state.currentProduct
+
+state.cart.push({
+name:p.name,
+price:p.price,
+qty:1,
+modifiers:[]
+})
+
+document.getElementById("modal").classList.remove("show")
+
+updateFloating()
+
+}
+
+window.addToCart=addToCart
