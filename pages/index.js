03f8986a3dea6dc045e32550ff6a47e673da3224
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-07 02:04:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,28 @@
-import { useEffect, useState } from "react"
-import liff from "@line/liff"
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
 
 export default function Home() {
-
-  const [name,setName] = useState("Loading...")
-  const [userId,setUserId] = useState("")
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    async function init() {
+      await liff.init({ liffId: "2009308319-2r1OXrGI" });
 
-    async function init(){
-
-      await liff.init({
-        liffId:"2009308319-2r1OXrGI"
-      })
-
-      if(!liff.isLoggedIn()){
-        liff.login()
-        return
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        const profile = await liff.getProfile();
+        setName(profile.displayName);
       }
-
-      const profile = await liff.getProfile()
-
-      setName(profile.displayName)
-      setUserId(profile.userId)
-
     }
 
-    init()
-
-  },[])
+    init();
+  }, []);
 
   return (
-
-    <div style={{padding:20,fontFamily:"sans-serif"}}>
-
+    <div style={{ padding: 40 }}>
       <h1>LIFF POS TEST</h1>
-
-      <p>Name: {name}</p>
-      <p>UserID: {userId}</p>
-
+      <p>User: {name}</p>
     </div>
-
-  )
-
+  );
 }
