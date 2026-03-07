bcf875a95f2e837f391026d6e55580fbfcc93ed5
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-07 08:55:46 +0700

    Create menu.js

---
+++ api/menu.js
@@ -0,0 +1,21 @@
+export default async function handler(req,res){
+
+ const r = await fetch(
+ "https://api.loyverse.com/v1.0/items",
+ {
+  headers:{
+   Authorization:"Bearer "+process.env.LOYVERSE_TOKEN
+  }
+ })
+
+ const data = await r.json()
+
+ const menu = data.items.map(i => ({
+   id:i.id,
+   name:i.item_name,
+   price:i.variants[0].price
+ }))
+
+ res.json(menu)
+
+}
