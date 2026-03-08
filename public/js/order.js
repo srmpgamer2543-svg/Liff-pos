60e7757ac017a73871b4244793095c9eea518461
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-08 12:22:34 +0700

    Update order.js

--- public/js/order.js
+++ public/js/order.js
@@ -1,1 +1,31 @@
+import { getCart, clearCart } from "./cart.js"
 
+export async function checkout(){
+
+const cart=getCart()
+
+if(cart.length===0){
+
+alert("cart empty")
+
+return
+
+}
+
+await fetch("/api/create-order",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({items:cart})
+
+})
+
+clearCart()
+
+alert("order sent")
+
+}
