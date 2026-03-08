b4d1728503956a72e5a49177ee919fb58fd8fc73
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-08 13:33:19 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,6 +2,7 @@ import { getMenu } from "./api.js"
 import { addToCart } from "./cart.js"
 
 let allMenu = []
+let categories = []
 
 export async function loadMenu(){
 
@@ -9,8 +10,33 @@ const menu = await getMenu()
 
 allMenu = menu || []
 
+prepareCategories()
+
 renderCategories()
+
 renderMenu(allMenu)
+
+}
+
+function prepareCategories(){
+
+const map = {}
+
+allMenu.forEach(item => {
+
+if(!map[item.category_id]){
+
+map[item.category_id] = {
+id: item.category_id,
+name: item.category_name || item.category_id,
+order: item.category_order || 0
+}
+
+}
+
+})
+
+categories = Object.values(map).sort((a,b)=>a.order-b.order)
 
 }
 
@@ -20,14 +46,6 @@ const container = document.getElementById("categories")
 if(!container) return
 
 container.innerHTML=""
-
-const categories = []
-
-allMenu.forEach(item => {
-if(!categories.includes(item.category_id)){
-categories.push(item.category_id)
-}
-})
 
 const allBtn = document.createElement("button")
 allBtn.innerText = "ทั้งหมด"
@@ -40,11 +58,13 @@ categories.forEach(cat => {
 
 const btn = document.createElement("button")
 
-btn.innerText = cat.substring(0,6)
+btn.innerText = cat.name
 
 btn.onclick = () => {
 
-const filtered = allMenu.filter(i => i.category_id === cat)
+const filtered = allMenu
+.filter(i => i.category_id === cat.id)
+.sort((a,b)=>a.name.localeCompare(b.name))
 
 renderMenu(filtered)
 
@@ -63,7 +83,17 @@ if(!container) return
 
 container.innerHTML=""
 
-menu.forEach(item => {
+const sorted = [...menu].sort((a,b)=>{
+
+if(a.category_order !== b.category_order){
+return (a.category_order||0) - (b.category_order||0)
+}
+
+return a.name.localeCompare(b.name)
+
+})
+
+sorted.forEach(item => {
 
 const div = document.createElement("div")
 
