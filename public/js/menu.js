292b83df45bede6d7c3b8c1553b9eceaa9d0a88b
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-08 12:08:47 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,1 +1,33 @@
+import { getMenu } from "./api.js"
+import { addToCart } from "./cart.js"
 
+export async function loadMenu(){
+
+const menu = await getMenu()
+
+const container = document.getElementById("menu")
+
+container.innerHTML=""
+
+menu.forEach(item=>{
+
+const div=document.createElement("div")
+
+div.className="item"
+
+div.innerHTML=`
+
+<img src="${item.image || ""}">
+<h3>${item.name}</h3>
+<p>${item.price}</p>
+<button>add</button>
+
+`
+
+div.querySelector("button").onclick=()=>addToCart(item)
+
+container.appendChild(div)
+
+})
+
+}
