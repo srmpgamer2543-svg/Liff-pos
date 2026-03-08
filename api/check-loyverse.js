397194fa69ba67da47866e6ff2f488d8120ad6ec
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-08 11:16:42 +0700

    Update check-loyverse.js

--- api/check-loyverse.js
+++ api/check-loyverse.js
@@ -1,26 +1,29 @@
 export default async function handler(req, res) {
 
-try {
+ try {
 
-const response = await fetch(
- "https://api.loyverse.com/v1.0/items",
- {
-  headers:{
-   Authorization: "Bearer 34b14f9c227247aaa1cb8af92921bf0b"
-  }
+  const response = await fetch(
+   "https://api.loyverse.com/v1.0/items",
+   {
+    method: "GET",
+    headers: {
+     "Authorization": "Bearer 34b14f9c227247aaa1cb8af92921bf0b",
+     "Content-Type": "application/json"
+    },
+    cache: "no-store"
+   }
+  )
+
+  const data = await response.json()
+
+  return res.status(200).json(data)
+
+ } catch (error) {
+
+  return res.status(500).json({
+   error: error.message
+  })
+
  }
-)
-
-const data = await response.json()
-
-return res.status(200).json(data)
-
-} catch (error) {
-
-return res.status(500).json({
- error: error.message
-})
-
-}
 
 }
