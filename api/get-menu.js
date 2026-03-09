d61701e5956505b786e4b183781de1ed76225a84
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-09 08:50:31 +0700

    Update get-menu.js

--- api/get-menu.js
+++ api/get-menu.js
@@ -5,27 +5,33 @@ const supabase = createClient(
  process.env.SUPABASE_KEY
 )
 
-export default async function handler(req, res) {
+export default async function handler(req,res){
 
- try {
+ try{
 
   const { data, error } = await supabase
    .from("menu")
-   .select("*")
+   .select(`
+    id,
+    name,
+    price,
+    image,
+    category_id,
+    categories (
+     id,
+     name
+    )
+   `)
 
-  if (error) {
-   return res.status(500).json({
-    error: error.message
-   })
+  if(error){
+   return res.status(500).json({ error:error.message })
   }
 
   res.status(200).json(data)
 
- } catch (err) {
+ }catch(err){
 
-  res.status(500).json({
-   error: err.message
-  })
+  res.status(500).json({ error:err.message })
 
  }
 
