38275a113a086f5ac118e51cd88a5949a04e155d
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-09 11:55:04 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,7 +2,6 @@ import { getMenu } from "./api.js"
 import { addToCart } from "./cart.js"
 
 let allMenu = []
-let categories = []
 
 export async function loadMenu(){
 
@@ -10,42 +9,15 @@ export async function loadMenu(){
 
   allMenu = menu || []
 
-  // ดึงหมวดหมู่จาก menu
-  const map = {}
-
-  allMenu.forEach(item => {
-    if(!map[item.category_id]){
-      map[item.category_id] = item.category_name
-    }
-  })
-
-  categories = Object.keys(map).map(id => ({
-    id,
-    name: map[id]
-  }))
-
-  // เรียงหมวดตามเลขหน้าชื่อ
-  categories.sort((a,b)=>{
-    const na = parseInt(a.name.split("_")[0])
-    const nb = parseInt(b.name.split("_")[0])
-    return na-nb
-  })
-
-  renderCategories()
-
-  // เรียงสินค้า
+  // เรียงตาม category_id ก่อน
   allMenu.sort((a,b)=>{
-    const ca = categories.find(c=>c.id===a.category_id)
-    const cb = categories.find(c=>c.id===b.category_id)
-
-    const na = parseInt(ca?.name.split("_")[0] || 0)
-    const nb = parseInt(cb?.name.split("_")[0] || 0)
-
-    if(na !== nb) return na-nb
-
+    if(a.category_id !== b.category_id){
+      return a.category_id.localeCompare(b.category_id)
+    }
     return a.name.localeCompare(b.name)
   })
 
+  renderCategories()
   renderMenu(allMenu)
 
 }
@@ -59,25 +31,30 @@ function renderCategories(){
 
   const allBtn = document.createElement("button")
   allBtn.innerText = "ทั้งหมด"
-  allBtn.onclick = () => renderMenu(allMenu)
+
+  allBtn.onclick = ()=>{
+    renderMenu(allMenu)
+  }
 
   container.appendChild(allBtn)
 
-  categories.forEach(cat => {
+  const categoryMap = {}
+
+  allMenu.forEach(item=>{
+    if(!categoryMap[item.category_id]){
+      categoryMap[item.category_id] = true
+    }
+  })
+
+  Object.keys(categoryMap).forEach(catId=>{
 
     const btn = document.createElement("button")
 
-    // ลบเลข 01_
-    const name = cat.name.replace(/^\d+_/, "")
+    btn.innerText = catId
 
-    btn.innerText = name
-
-    btn.onclick = () => {
-
-      const filtered = allMenu.filter(i => i.category_id === cat.id)
-
+    btn.onclick = ()=>{
+      const filtered = allMenu.filter(i=>i.category_id===catId)
       renderMenu(filtered)
-
     }
 
     container.appendChild(btn)
@@ -93,20 +70,22 @@ function renderMenu(menu){
 
   container.innerHTML=""
 
-  menu.forEach(item => {
+  menu.forEach(item=>{
 
     const div = document.createElement("div")
 
-    div.className = "item"
+    div.className="item"
 
-    div.innerHTML = `
+    div.innerHTML=`
       <img src="${item.image || ""}">
       <h3>${item.name}</h3>
       <p>${item.price}</p>
       <button>เพิ่ม</button>
     `
 
-    div.querySelector("button").onclick = () => addToCart(item)
+    div.querySelector("button").onclick=()=>{
+      addToCart(item)
+    }
 
     container.appendChild(div)
 
