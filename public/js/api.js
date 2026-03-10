9769fde56194f87a07e6a23073d2407c601dfcbf
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-10 08:27:09 +0700

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,16 @@
+let menuCache=null
+let categoriesCache=null
+
 export async function getMenu(){
+
+ if(menuCache){
+  return menuCache
+ }
 
  const res = await fetch("/api/get-menu")
  const data = await res.json()
+
+ menuCache=data
 
  return data
 
@@ -9,8 +18,14 @@ export async function getMenu(){
 
 export async function getCategories(){
 
+ if(categoriesCache){
+  return categoriesCache
+ }
+
  const res = await fetch("/api/get-categories")
  const data = await res.json()
+
+ categoriesCache=data
 
  return data
 
