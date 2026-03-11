faff0f917f0008097e9c905207a38ea8fff0fd67
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-12 00:41:43 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,10 @@
 import { loadMenu } from "./menu.js"
 import { toggleCart } from "./ui.js"
-import { checkout } from "./order.js"
 
 window.toggleCart = toggleCart
-window.checkout = checkout
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-loadMenu()
+ loadMenu()
 
 })
