3117dee0fa583bd2905826598da13608a1825edc
srmpgamer2543-svg <srmpgamer2543@gmail.com>
2026-03-11 10:31:30 +0700

    Delete api/test.js

--- api/test.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "API working",
-    loyverse: process.env.LOYVERSE_API_KEY ? true : false,
-    supabase: process.env.SUPABASE_URL ? true : false
-  });
-}
